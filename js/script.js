967ecc140dbd1fc83b3f900f2c930ae7037ec42b
jonnebonde <jonnebonde@gmail.com>
2022-01-20 11:19:18 +0100

    enter

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// question 1
+
+const hello = document.querySelector("h3");
+hello.innerHTML = "Hello";
+
+
+// question 2
+
+
+const margin = document.querySelectorAll("h4");
+
+for(let i = 0; i < margin.length; i++) {
+    margin[i].style.marginBottom = "-20px";
+}
+
+// question 3
+
+const secondH4 = document.querySelector("h4.two");
+secondH4.style.backgroundColor = "red";
+
+// question 4
+
+
+
